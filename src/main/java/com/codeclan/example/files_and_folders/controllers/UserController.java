a22fb0eae748715050a9f71aa0b84e93bbf53884
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-08-04 17:02:57 +0100

    Add routes for UserController

--- src/main/java/com/codeclan/example/files_and_folders/controllers/UserController.java
+++ src/main/java/com/codeclan/example/files_and_folders/controllers/UserController.java
@@ -1,4 +1,30 @@
 package com.codeclan.example.files_and_folders.controllers;
 
+import com.codeclan.example.files_and_folders.models.User;
+import com.codeclan.example.files_and_folders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity<Optional<User>> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
 }
