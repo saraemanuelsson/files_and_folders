262c07cb3868fd8a56c30d1aebc7c0d7be697c3b
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-08-04 16:57:49 +0100

    Add routes in FileController

--- src/main/java/com/codeclan/example/files_and_folders/controllers/FileController.java
+++ src/main/java/com/codeclan/example/files_and_folders/controllers/FileController.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.files_and_folders.controllers;
 
+import com.codeclan.example.files_and_folders.models.File;
+import com.codeclan.example.files_and_folders.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class FileController {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles() {
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity<Optional<File>> getFile(@PathVariable Long id) {
+        return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
+    }
+
 }
